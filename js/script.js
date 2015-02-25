bef267a84b69307f0b054951a01fcef2b23a7eda
marcelxx <mkipp@uos.de>
2015-02-25 16:31:01 +0100

    myUOS menu js
    html code wird per js eingefügt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+var $insertHTML = $('<div id="slider" class="slideroff-slider"><div class="slider-wrapper"><a href="http://www.uos.de"><img style="float:left;" height="50px" src="data/menu/img/header_logo.gif" alt="logo"></a><ul><li id="sliderstudiumtrigger" onclick="ultrigger_01()"><b>Studium</b><ul id="ultarget_01"><li>Stud.IP</li><li>OPIuM</li><li>Uni Bibliothek</li><li>Lernfunk</li><li>Alumni</li></ul></li><li id="sliderdienstleistungtrigger" onclick="ultrigger_02()"><b>Dienstleistungen</b><ul id="ultarget_02"><li>E-Mail</li><li>MyRz</li><li>MyShare</li></ul></li></ul><div class="username-menu"> Max Mustermann &nbsp; &nbsp; <i title="myUOS-Portal" class="fa fa-home"></i> &nbsp;<i title="Logout" class="fa fa-sign-out"></i> </div></div></div><div onclick="slideron()" class="black-overlay" id="overlay"><div class="close-icon"><i class="fa fa-times"></i> </div></div><div align="center" onclick="slideron()" class="ausklappen" id="ausklappen"><img src="data/menu/img/logo_myuos.png" alt="myUOS-logo"></div>');
+
 jQuery(function() {
+    jQuery("body").prepend($insertHTML);
     jQuery("#sliderstudiumtrigger, #sliderstudium").hover(function() {
         jQuery("#sliderstudium").toggleClass("slideron-studium");
         jQuery("#navbar").toggleClass("navbar-on");
@@ -12,6 +15,7 @@ jQuery(function() {
     jQuery("#sliderdienstleistung").hover(function() {
         jQuery("#sliderdienstleistungtrigger").toggleClass("slideroff-slider-hovermenu");
     });
+
 });
 
 function slideron() {
